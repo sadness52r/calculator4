d3834fa765cd1a80a7822b406726f8030c5b2433
sadness52r <bukin.k2017@gmail.com>
2024-06-06 20:42:53 +0300

    add multiply, division, division by module functions

--- src/calc_methods.py
+++ src/calc_methods.py
@@ -6,3 +6,19 @@ def summ(a: int, b: int) -> int:
 # sub func: 1 - 1 -> 0
 def sub(a: int, b: int) -> int:
     return a - b
+
+
+def mult(a: int, b: int) -> int:
+    return a * b
+
+
+def div(a: int, b: int) -> float:
+    if b == 0:
+        return 999999999999999999999
+    return a / b
+
+
+def mod(a: int, b: int) -> int:
+    if b == 0:
+        return 999999999999999999998
+    return a % b
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,5 @@
 import pytest
-from src.calc_methods import summ, sub
+from src.calc_methods import summ, sub, mult, div, mod
 
 
 @pytest.mark.parametrize(
@@ -26,3 +26,45 @@ def test_summ(a, b, res):
 )
 def test_sub(a, b, res):
     assert sub(a, b) == res
+
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'res'), [
+        (2, 3, 6),
+        (0, 0, 0),
+        (-4, 6, -24),
+        (-8, -8, 64),
+        (-25, 25, -625),
+        (1, 1, 1),
+    ]
+)
+def test_mult(a, b, res):
+    assert mult(a, b) == res
+
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'res'), [
+        (2, 3, 2 / 3),
+        (0, 0, 999999999999999999999),
+        (0, 5, 0),
+        (-4, 6, -4 / 6),
+        (-8, -8, 1),
+        (-25, 25, -1),
+    ]
+)
+def test_div(a, b, res):
+    assert div(a, b) == res
+
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'res'), [
+        (2, 3, 2),
+        (0, 0, 999999999999999999998),
+        (0, 5, 0),
+        (-4, 6, -4 % 6),
+        (-8, -8, -8 % -8),
+        (-25, 25, -25 % 25),
+    ]
+)
+def test_mod(a, b, res):
+    assert mod(a, b) == res
