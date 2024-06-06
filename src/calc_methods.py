4cabcc46124f01713cfecda3ed0aa4c624ea148f
sadness52r <bukin.k2017@gmail.com>
2024-06-06 20:44:17 +0300

    add comments

--- src/calc_methods.py
+++ src/calc_methods.py
@@ -8,16 +8,19 @@ def sub(a: int, b: int) -> int:
     return a - b
 
 
+# mult func: 2 * 2 -> 4
 def mult(a: int, b: int) -> int:
     return a * b
 
 
+# div func: 6 / 3 -> 2
 def div(a: int, b: int) -> float:
     if b == 0:
         return 999999999999999999999
     return a / b
 
 
+# mod func: 6 % 4 -> 2
 def mod(a: int, b: int) -> int:
     if b == 0:
         return 999999999999999999998
